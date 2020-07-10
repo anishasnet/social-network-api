98d6ab4d8edbfca01d172f25647c214736da90dd
anishasnet <anishasnet@gmail.com>
2020-07-10 08:49:53 -0700

    Connected mongoose to create database

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
 mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
